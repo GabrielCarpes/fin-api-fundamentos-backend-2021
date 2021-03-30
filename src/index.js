30177c4505bc160d35a3b98b126270fd431340bd
Gabriel Carpes <gabrcarpes@gmail.com>
2021-03-30 01:38:09 -0300

    chore: first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) =>{
-  return response.json({msg: 'OK!'});
+  return response.json({msg: 'OK!!'});
 })
 
 app.listen(3000)
